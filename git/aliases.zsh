eb2e70529363580de385bf4b04f6f76bdf27beb4
Jan van der Pas <janvanderpas@gmail.com>
2014-07-15 23:49:13 +0200

    Add the dotfiles for Git

---
+++ git/aliases.zsh
@@ -0,0 +1,8 @@
+# Aliases for Git
+alias st='git status'
+alias co='git checkout'
+alias br='git branch'
+alias add='git add'
+alias com='git commit'
+alias diff='git diff'
+alias diffs='git diff --staged'
