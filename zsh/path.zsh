9fb286d7ef410d51669cc41d3bae4074db36e184
Jan van der Pas <janvanderpas@gmail.com>
2014-07-16 00:15:28 +0200

    Change the PATH ENV variable to give the right precedence:
    1. The ./bin directory of the pwd
    2. The /usr/local/bin and sbin directories
    3. The DOTFILES ENV variable ./bin directory
    4. The PATH ENV variable directories

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,7 +1,2 @@
 # PATH for ZSH
-
-# good
-export PATH=/usr/local/bin:$PATH/usr/bin:/bin:/usr/sbin:/sbin
-
-# beter?
-# export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
