6ba2f20da0b0c3e6d45bea9208c0fbed847255c9
Jan van der Pas <janvanderpas@gmail.com>
2014-07-15 23:53:43 +0200

    Move the system specify environment variables from the ZSH dotfiles to the system dotfiles

---
+++ system/env.zsh
@@ -0,0 +1,9 @@
+# Environment variables for system
+
+# Language environment
+export LANGUAGE="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
+# Editor
+export EDITOR='subl'
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -8,11 +8,3 @@ RPROMPT="[`~/.rvm/bin/rvm-prompt`]"
 
 # Auto-update
 export UPDATE_ZSH_DAYS=3
-
-# Language environment
-export LANGUAGE="en_US.UTF-8"
-export LANG="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-
-# Editor
-export EDITOR='subl'
