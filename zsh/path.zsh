a2c93bc2b7789bc1b78870905ff57c58241e042e
Jan van der Pas <janvanderpas@gmail.com>
2014-07-16 01:27:51 +0200

    Load RVM into the PATH ENV variable, after the ZSH PATH ENV variable.
    Note: Fixes PATH issues with Homebrew and RVM

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,16 @@
 # PATH for ZSH
+#
+# Load order precedence:
+# 1. The ./bin directory of RVM gems
+# 2. The ./bin directory of RVM rubies
+# 3. The ./bin directory of the present working directory (e.g. subl)
+# 4. The /usr/local/bin and /usr/local/sbin directories (e.g. homebrew)
+# 5. The ./bin directory of DOTFILES ENV variable
+# 6. The /usr/bin and /usr/sbin directories of PATH ENV variable
+# 7. The ./bin directory of RVM
+
+# Load ZSH into PATH
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+
+# Load RVM into PATH as a function (for scripting)
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
