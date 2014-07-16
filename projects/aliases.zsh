e20287b2da7aedaeda6934990f14aec02a868062
Jan van der Pas <jvanderpas@digidentity.eu>
2014-07-16 10:28:06 +0200

    Add the aliases dotfile for the project dotfiles, which automatically set the aliases for a project directory

---
+++ projects/aliases.zsh
@@ -0,0 +1,9 @@
+# Aliases for projects
+project_aliases=($(find -P "$PROJECTS" -maxdepth 3 -name '*.alias'))
+
+for project_alias in $project_aliases; do
+  project_dir=$(dirname $project_alias)
+  project_name=$(basename $project_dir)
+
+  alias $project_name="cd $project_dir"
+done
