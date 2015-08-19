693ebdaf41a99572727d99d2fe7cb586a95c8085
Jan van der Pas <jvanderpas@digidentity.eu>
2015-08-19 08:28:58 +0200

    Add the alias 'be' which aliasses to 'bundle exec'

--- rails/aliases.zsh
+++ rails/aliases.zsh
@@ -5,3 +5,6 @@ alias rdr='rake db:reset'
 alias rdrt='rake db:reset RAILS_ENV=test'
 alias rdrh='rake db:drop; rake db:create; rake db:migrate'
 alias rdrht='rake db:drop RAILS_ENV=test; rake db:create RAILS_ENV=test; rake db:migrate RAILS_ENV=test'
+
+# Aliases for Bundler
+alias be='bundle exec'
