51cbc4c842ebde96006ce2826c832e7e2fae9e37
Jan van der Pas <jvanderpas@digidentity.eu>
2016-02-26 14:19:33 +0100

    Add a Vagrant helper command ‘vagrant_application_command’, which runs a given command in the directory of a Rails application running in a Vagrant VM.

--- vagrant/helpers.zsh
+++ vagrant/helpers.zsh
@@ -10,3 +10,17 @@ vagrant_application_log() {
 
   vagrant ssh -c "cd /vagrant/$1; tail -f log/development.log"
 }
+
+# Run command in directory of a Rails application
+#
+# Usage:
+#  $ vagrant_application_command demo_app be rake db:migrate
+#
+vagrant_application_command() {
+  [[ -n $1 ]] || { echo -e "\033[0;31mPlease specify the application!\033[0m"; return 1; }
+
+  APP="$1"
+  shift
+
+  vagrant ssh -c "cd /vagrant/$APP; $@"
+}
